765ba9f4fb2a3d170a29b9dbb3d24547e3bd311c
LegendsZ <akkshai@yahoo.com>
2025-10-20 22:04:28 -0400

    init setup

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define VERSION 1.0.0
+#define TERMINAL //define GUI on gui impl
+
+int main(int argc, char* argv[]){
+    std::cout << "Starting ListOBranch...";
+    std::cout << "OK!\n"
+    
+    
+    
+    std::cout << "Ending ListOBranch..."
+    std::cout << "OK!\n"
+    return 0;
+}
